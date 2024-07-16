b7866c19efac02fd01fb280e6cfd6734900a3aac
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-07-16 23:59:35 +0700

    finish button add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,31 +1,33 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import '../util/todo_body.dart';
 
-class HomePage extends StatefulWidget{
-  const HomePage ({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
-
+class _HomePageState extends State<HomePage> {
   List todoList = [
     ["Create Notes", false],
     ["Create Notes", false]
   ];
 
-void checkboxChanged(bool? value, int index) {
-  setState(() {
-    todoList[index][1] = !todoList [index] [1];
-  });
-}
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
 
-void createNewTask(){
-  showDialog(context: context, builder: (context));
-}
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,18 +39,16 @@ void createNewTask(){
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: createNewTask,
-      child: Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
       body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return TodoBody(
-            taskName: todoList[index][0],
-            taskComplete: todoList[index][1],
-            onChanged: (value) => checkboxChanged(value, index)
-          );
-        }
-      ),
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return TodoBody(
+                taskName: todoList[index][0],
+                taskComplete: todoList[index][1],
+                onChanged: (value) => checkboxChanged(value, index));
+          }),
     );
   }
 }
